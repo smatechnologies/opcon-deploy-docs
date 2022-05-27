70d1f57925a17dbc4e0c620882cc55b5e040d0d5
Ryan Dimick <rdimick@smatechnologies.com>
2022-05-27 16:15:57 -0500

    Initialize repository

---
+++ README.md
@@ -0,0 +1,24 @@
+# SMA OpCon Deploy Docs
+
+This repository contains the content behind the SMA OpCon Deploy documentation hosted at <https://help.smatechnologies.com/opcon/deploy>.
+
+## Disclaimer
+
+No Support and No Warranty are provided by SMA Technologies for this project and related material. The use of this project's files is on your own risk.
+
+SMA Technologies assumes no liability for damage caused by the usage of any of the files offered here via this Github repository.
+
+## License
+
+Copyright 2021 SMA Technologies
+
+This SMA OpCon Deploy documentation is [Creative Commons licensed](LICENSE).
+
+## Contribution
+
+Please see <https://help.smatechnologies.com/opcon/contributor-guide> for information on how to contribute.
+
+## Code of Conduct
+
+[![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-v2.0%20adopted-ff69b4.svg)](code-of-conduct.md)
+SMA Technologies has adopted the [Contributor Covenant](CODE_OF_CONDUCT.md) as its Code of Conduct, and we expect project participants to adhere to it. Please read the [full text](CODE_OF_CONDUCT.md) so that you can understand what actions will and will not be tolerated.
---
+++ docusaurus.config.js
@@ -0,0 +1,53 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'SMA Technologies Help',
+  tagline: 'OpCon Deploy',
+  url: 'https://help.smatechnologies.com',
+  baseUrl: '/opcon/deploy/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'smatechnologies',
+  projectName: 'opcon-deploy-docs',
+  themeConfig: {
+    navbar: {
+      title: 'Help',
+      logo: {
+        alt: 'SMA Technologies Help Logo',
+        src: 'img/logo.svg',
+        href: 'https://help.smatechnologies.com',
+      },
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()} SMA Technologies.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          breadcrumbs: false,
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/smatechnologies/opcon-deploy-docs/blob/main',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+        gtag: {
+          trackingID: 'G-7XYMFXX81Y',
+        },
+      },
+    ],
+  ],
+  plugins: [
+    [
+      require.resolve('@cmfcmf/docusaurus-search-local'), 
+      {
+      }
+    ],
+  ],
+};
